ae7f19bed9b2ef4b6ce3f51fe8f9a76d87c93221
Jose Felix Gomez <jfgomez86@gmail.com>
2009-02-24 20:26:19 -0500

    removed EventMachine code

--- application.rb
+++ application.rb
@@ -18,18 +18,18 @@ DataMapper.auto_upgrade!
 
 enable :sessions
 
-unless defined? EM
+#unless defined? EM
   Thread.new do
     while true
       clean_chat_rooms
       sleep(60)
     end
   end
-else
-  EM.add_periodic_timer(60) do
-    clean_chat_rooms
-  end
-end
+#else
+  #EM.add_periodic_timer(60) do
+    #clean_chat_rooms
+  #end
+#end
 
 def clean_chat_rooms
   Chat.all.each do |chat|
